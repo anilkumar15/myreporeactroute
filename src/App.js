baa77d838b10f8d1490b6902347031fbd3f7e7b0
Mahesh Sabnis <sabnis_m@hotmail.com>
2020-11-04 22:08:54 +0530

    first app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
 import React from 'react';
-import logo from './logo.svg';
+import {Route, Link, Switch,Redirect} from 'react-router-dom';
+ 
 import './App.css';
+import './../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import CreateProductComponent from './components/CreateProductComponent';
+import EditProductComponent from './components/EditProductComponent';
+import ProductListComponent from './components/ProductListComponent';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="container">
+                <h1>The React Routing Application</h1>
+                <table className="table table-bordered table-striped table-danger">
+                   
+                  <tbody>
+                     <tr>
+                        <td>
+                            <Link to="/">List Products</Link>
+                        </td>
+                          <td>
+                            <Link to="/create">Create Product</Link>
+                        </td> 
+                     </tr>
+                  </tbody>
+                </table>
+                {/* Define Route Table Here */}
+                <div>
+                    <Switch>
+                        <Route exact path="/" component={ProductListComponent}></Route>
+                        <Route exact path="/create" component={CreateProductComponent}></Route>
+                        <Route exact path="/edit/:id" component={EditProductComponent}></Route>
+                       
+                        <Redirect to="/"/>
+                    </Switch>
+                </div>
+            </div>
   );
 }
 
---
+++ src/components/ProductListComponent.jsx
@@ -0,0 +1,77 @@
+import React, { useState, useEffect } from 'react';
+import { HttpService } from '../services/httpservice';
+import {Link} from 'react-router-dom';
+
+const ProductListComponent=(props)=> {
+    const [products, updateProducts] = useState([]);
+    const service = new HttpService();
+    useEffect(()=>{
+        service.getData()
+            .then(response=>{
+                updateProducts(response.data);
+            })
+            .catch(error=>{
+                console.log(`Error Occured ${error}`);
+            }); 
+         
+    }, []);
+
+
+    const remove=(id)=>{
+        service.deleteData(id).then(response=>{
+           window.location.reload();
+        })
+        .catch(error=>{
+            console.log(`Error Occured ${error}`);
+        }); 
+    }
+
+    if(products.length >0 ){
+    return (
+        <table className="table table-bordered table-striped">
+            <thead>
+                <tr>
+                    {
+                        Object.keys(products[0]).map((col,idx)=> (
+                            <th key={idx}>{col}</th>
+                        ))
+                    }
+                    <th></th>
+                    <th></th>
+                </tr>
+            </thead>
+            <tbody>
+                 {
+                     products.map((prd,index)=>(
+                         <tr key={index}>
+                          {
+                            Object.keys(prd).map((col,idx)=> (
+                                <td key={idx}>{prd[col]}</td>
+                            ))
+                            
+                          }
+                          <td>
+                           <button className="btn btn-warning">
+                                    <Link to={`/edit/${prd.ProductRowId}`} >Edit</Link>
+                           </button>
+                           
+                          </td>
+                          <td>
+                             <input type="button" className="btn btn-danger" value="Delete"
+                              onClick={()=>{remove(prd.ProductRowId)}}/>
+                          </td>
+                         </tr>
+                     ))
+                 }
+            </tbody>
+        </table>
+                 
+        )  } else {
+            return (
+                <div>No Record Found</div>
+            )
+        }
+    
+};
+
+export default ProductListComponent;
